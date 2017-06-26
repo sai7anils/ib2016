f88d1f93259560f7a82bfaa337da4293bda24d92
sai7anils <sai7anils@gmail.com>
2017-06-26 23:19:48 +0530

    modified the signup code.
    old : 
    def configure_sign_up_params
         devise_parameter_sanitizer.for(:sign_up) << [:user_type, :username]
      end
    new :
    def configure_sign_up_params    
         devise_parameter_sanitizer.permit(:sign_up, keys: [:user_type, :username])
      end

--- app/controllers/user/registrations_controller.rb
+++ app/controllers/user/registrations_controller.rb
@@ -135,8 +135,8 @@ class User::RegistrationsController < Devise::RegistrationsController
   # protected
 
   # If you have extra params to permit, append them to the sanitizer.
-  def configure_sign_up_params
-     devise_parameter_sanitizer.for(:sign_up) << [:user_type, :username]
+  def configure_sign_up_params    
+     devise_parameter_sanitizer.permit(:sign_up, keys: [:user_type, :username])
   end
 
   # If you have extra params to permit, append them to the sanitizer.
